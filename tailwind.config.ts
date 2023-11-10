034438b429c38cc8722e6ab442ed1b219ebdec6c
Karl George <karlgeorge87@gmail.com>
2023-11-10 12:27:04 +0100

    Change background colour of sheet component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ module.exports = {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
+        background: '#f9f9f9',
         foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
