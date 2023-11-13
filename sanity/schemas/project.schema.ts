c1ac4e3cdebf0abaf00caff8cab97d7320ebee4f
Karl George <karlgeorge87@gmail.com>
2023-11-13 11:45:19 +0100

    Add Schemas

---
+++ sanity/schemas/project.schema.ts
@@ -0,0 +1,29 @@
+const schema = {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'codeLink',
+      title: 'Code Link',
+      type: 'url',
+    },
+    {
+      name: 'liveLink',
+      title: 'Live Link',
+      type: 'url',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+  ],
+};
+
+export default schema;
