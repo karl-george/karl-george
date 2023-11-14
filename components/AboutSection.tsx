a01c0b170af0b5a0a2c731c51a44322a4df43ea2
Karl George <karlgeorge87@gmail.com>
2023-11-14 09:59:00 +0100

    Finish styling about section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,24 +1,34 @@
+import Image from 'next/image';
+
 interface AboutSectionProps {}
 
 function AboutSection({}: AboutSectionProps) {
   return (
-    <section>
-      <div className='m-14'>
-        <h3 className='text-5xl font-bold text-center'>About</h3>
-      </div>
-      <div className='flex flex-col lg:flex-row'>
-        {/* <Image /> */}
-        <div>
-          <p>
-            As a Junior Front-End Developer, I possess an impressive arsenal of
-            skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel
-            in designing and maintaining responsive websites that offer a smooth
-            user experience. My expertise lies in crafting dynamic, engaging
-            interfaces through writing clean and optimized code and utilizing
-            cutting-edge development tools and techniques. I am also a team
-            player who thrives in collaborating with cross-functional teams to
-            produce outstanding web applications.
-          </p>
+    <section className='py-28' id='about'>
+      <div className='container'>
+        <div className='mb-20'>
+          <h3 className='text-5xl font-bold text-center'>About</h3>
+        </div>
+        <div className='flex flex-col lg:flex-row gap-28 justify-center items-center'>
+          {/* Image  */}
+          <Image
+            src='/about-img.jpg'
+            width={450}
+            height={450}
+            alt='about'
+            className='rounded-2xl'
+          />
+          {/* Content */}
+          <div className='max-w-lg'>
+            <p className='text-lg'>
+              As a Junior Front-End Developer, I am experienced in using HTML,
+              CSS, JavaScript, TypeScript, React, Tailwind and SCSS. I enjoy
+              designing and developing responsive websites that offer a smooth
+              user experience. My expertise lies in crafting dynamic, engaging
+              interfaces through writing clean and optimized code and utilizing
+              cutting-edge development tools and techniques.
+            </p>
+          </div>
         </div>
       </div>
     </section>
