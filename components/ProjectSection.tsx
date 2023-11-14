fb1969146049d9ac334de34b9390cbed21d42339
Karl George <karlgeorge87@gmail.com>
2023-11-14 11:50:56 +0100

    Update mobile widths

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -13,7 +13,7 @@ async function ProjectSection({}: ProjectSectionProps) {
           <h3 className='text-5xl font-bold text-center'>Projects</h3>
         </div>
         {/* Projects Grid */}
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[500px] md:w-full mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[500px] md:w-full md:max-w-none mx-auto'>
           {projects.map((project: Project) => (
             <ProjectCard project={project} key={project._id} />
           ))}
