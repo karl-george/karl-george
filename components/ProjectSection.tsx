0c351db32fc3bfa5c7e01f37dc69cb78c9902a9f
Karl George <karlgeorge87@gmail.com>
2023-11-14 11:23:43 +0100

    Update project section

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,10 +1,23 @@
+import { getProjects } from '@/sanity/actions';
+import ProjectCard from './ProjectCard';
+
 interface ProjectSectionProps {}
 
-function ProjectSection({}: ProjectSectionProps) {
+async function ProjectSection({}: ProjectSectionProps) {
+  const projects = await getProjects();
+
   return (
-    <section>
-      <div className='m-14'>
-        <h3 className='text-5xl font-bold text-center'>Projects</h3>
+    <section className='py-28 bg-bgDark' id='projects'>
+      <div className='container'>
+        <div className='mb-20'>
+          <h3 className='text-5xl font-bold text-center'>Projects</h3>
+        </div>
+        {/* Projects Grid */}
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[500px] md:w-full mx-auto'>
+          {projects.map((project: Project) => (
+            <ProjectCard project={project} key={project._id} />
+          ))}
+        </div>
       </div>
     </section>
   );
