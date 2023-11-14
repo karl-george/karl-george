ae4488abc0900f9c8209b8e03b1d2ff07a733782
Karl George <karlgeorge87@gmail.com>
2023-11-14 11:52:35 +0100

    Update contact header

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -2,9 +2,11 @@ interface ContactSectionProps {}
 
 function ContactSection({}: ContactSectionProps) {
   return (
-    <section>
-      <div className='m-14'>
-        <h3 className='text-5xl font-bold text-center'>Contact</h3>
+    <section className='py-28 bg-bgDark' id='contact'>
+      <div className='container'>
+        <div className='mb-20'>
+          <h3 className='text-5xl font-bold text-center'>Contact</h3>
+        </div>
       </div>
     </section>
   );
