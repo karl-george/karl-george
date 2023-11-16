66ba4b3099f53bb42bf10c310b24c87e39b5dfb3
Karl George <karlgeorge87@gmail.com>
2023-11-16 10:08:05 +0100

    Add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link';
+import { Github, Linkedin } from 'lucide-react';
+
 interface FooterProps {}
 
 function Footer({}: FooterProps) {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-slate-800 text-white'>
+      <div className='container flex flex-row items-center justify-center p-4 gap-24'>
+        <div>
+          <p>Karl George © 2023</p>
+        </div>
+        <div className='flex flex-row gap-4'>
+          <Link
+            href='https://github.com/karl-george'
+            target='_blank'
+            referrerPolicy='no-referrer'
+            className='hover:text-accent'
+          >
+            <Github />
+          </Link>
+          <Link
+            href='https://www.linkedin.com/in/karlgeorge87/'
+            target='_blank'
+            referrerPolicy='no-referrer'
+            className='hover:text-accent'
+          >
+            <Linkedin />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
